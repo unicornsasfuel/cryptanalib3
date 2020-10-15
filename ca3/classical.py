efac7d5e1faa3ca5bd784cead400a873e54fc51b
unicornsasfuel <dan.crowley@gmail.com>
2020-10-15 11:57:47 -0500

    move constants to helpers

--- ca3/classical.py
+++ ca3/classical.py
@@ -12,9 +12,6 @@ from . import frequency
 import operator
 from math import sqrt
 
-lowercase_letters = [b'a', b'b', b'c', b'd', b'e', b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's', b't', b'u', b'v', b'w', b'x', b'y', b'z']
-uppercase_letters = [b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I', b'J', b'K', b'L', b'M', b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W', b'X', b'Y', b'Z']
-digits = [b'1', b'2', b'3', b'4', b'5', b'6', b'6', b'7', b'8', b'9', b'0']
 # -------------------------
 # Classical cryptography functions
 # 
